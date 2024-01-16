06eb25e3462ce582ee66d6ec15c19e83ba968364
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-16 11:08:02 +0530

    Add files via upload

---
+++ data_type_casting.py
@@ -0,0 +1,26 @@
+# Data Type casting
+# Type
+num = 52
+print(type(num))
+
+num = 52.5
+print(type(num))
+
+num = "52.44"
+print(type(num))
+
+# Data type casting
+num = 52
+num1 = str(num)
+print(type(num1))
+
+num = 52.5
+num2 = int(num)
+print(type(num2))
+
+num = 52
+num3 = float(num)
+print(type(num3))
+
+
+
